8457c44293148501e618b06dc8e52a061bf5d8f2
Khushi bedar <121684264+khushibedar@users.noreply.github.com>
2024-04-05 18:40:24 +0530

    Update product.sql

--- models/product.sql
+++ models/product.sql
@@ -1,8 +1,7 @@
 select 
-    id as product_id,
-    currency_code as cr_code,
-    status as product_staus,
-    name as product_name
-from product
+    *
+from {{ sources('source1' , 'test2')}}
+
+    
 
     
